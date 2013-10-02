1992b65764014d120db51a3e312c51830cd4fa6b
Gordon Inggs <gordon.inggs@gmail.com>
2013-10-02 21:45:02 +0200

    Big improvement to script. The directory structure from the source is now preserved in the destination.

--- backup.py
+++ backup.py
@@ -6,6 +6,5 @@ source = "~/Documents/workspace/mac_backup_script"
 dest = "~/Dropbox/scratch/backup_test"
 
 files = []
-for ft in file_types:
-    if not(subprocess.call("find %s/*.%s"%(source,ft),shell=True)): files.extend(subprocess.check_output("find %s/*.%s"%(source,ft),shell=True).split("\n")[:-1])
-for f in files: subprocess.check_output("rsync -a %s %s/"%(f,dest),shell=True)
+for ft in file_types: files.extend(subprocess.check_output("cd %s;find . | grep [A-Z]*\.%s$"%(source,ft),shell=True).split("\n")[:-1])
+for f in files: subprocess.check_output("cd %s;rsync -Ravz %s %s"%(source,f,dest),shell=True)
