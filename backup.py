4c5c78e6d2f8f77d5cfb267aa165d786eccabd2c
Gordon Inggs <gordon.inggs@gmail.com>
2014-10-26 13:22:57 +0000

    Annual update

--- backup.py
+++ backup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 import subprocess
 
-file_types = ["doc","docx","pdf"]
-source = "~/Documents/workspace/mac_backup_script"
+file_types = ["doc"]
+max_size = 1024*1024*50
+
+source = "~"
 dest = "~/Dropbox/scratch/backup_test"
 
-files = []
-for ft in file_types: files.extend(subprocess.check_output("cd %s;find . | grep [A-Z]*\.%s$"%(source,ft),shell=True).split("\n")[:-1])
-for f in files: subprocess.check_output("cd %s;rsync -Ravz %s %s"%(source,f,dest),shell=True)
+for fs in file_types:
+    subprocess.call("rsync -auvm --max-size=%d --include=\"*/\" --include=\"*.%s\" --exclude=\"*\" %s %s"%(max_size,fs,source,dest),shell=True)
